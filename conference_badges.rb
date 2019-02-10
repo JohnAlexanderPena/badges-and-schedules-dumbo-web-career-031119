f2ce9f4e9946168c1bfd43ddb196f4e977e0d942
John Pena <jpena91@gmail.com>
2019-02-10 21:48:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,4 +3,12 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(name_array)
-  for i in name_array do
+  badge_messages = []
+  for name in name_array do 
+    badge_messages.push(badge_maker(name))
+  end
+  badge_messages
+end
+end 
+    
+    
