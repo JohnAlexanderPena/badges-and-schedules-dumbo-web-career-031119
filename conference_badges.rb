be78c0644782a8c91273c6a58ca4e6f1f33ffb00
John Pena <jpena91@gmail.com>
2019-02-10 22:02:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,8 +21,7 @@ def assign_rooms(speakers)
 end 
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
-
+  p batch_badge_creator(attendees)
+  p assign_rooms
 end 
     
