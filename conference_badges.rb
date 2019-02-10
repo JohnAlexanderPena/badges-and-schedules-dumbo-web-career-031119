cec9cb075ac149d4c9d326cebe74b0754687b39e
John Pena <jpena91@gmail.com>
2019-02-10 21:52:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,9 +7,11 @@ def batch_badge_creator(name_array)
   for name in name_array do 
     badge_messages.push(badge_maker(name))
   end
-  badge_messages
+  badge_message
 end
 
 def assign_rooms(speakers)
+  
+end 
     
     
